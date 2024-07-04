5ca52d14eccb2e68fc23ed9ec88d0374b457155c
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-07-05 00:28:56 +0300

    Add name input field with validation in sign up form

--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -1,20 +1,35 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 
-
 export default function SignUpPage() {
-      const {
-        register,
-        handleSubmit,
-        watch,
-        formState: { errors },
-      } = useForm();
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
   return (
     <div>
       <form>
-
-
-
+        <div>
+          <label htmlFor="name">
+            <p>Name</p>
+            <input
+              type="text"
+              placeholder="Your Full Name"
+              {...register("name", {
+                required: "Please enter your name.",
+                minLength: {
+                  value: 3,
+                  message: "Your name should be at least 3 characters long.",
+                },
+              })}
+            ></input>
+            {typeof errors.name?.message === "string" && (
+              <p className="text-red-500">{errors.name.message}</p>
+            )}
+          </label>
+        </div>
       </form>
     </div>
   );
