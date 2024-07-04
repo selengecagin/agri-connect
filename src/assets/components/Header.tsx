0b3bf48cb90eb4f9091109fbfa99571d1d2afbf9
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-07-04 14:43:43 +0300

    Style header component

--- src/assets/components/Header.tsx
+++ src/assets/components/Header.tsx
@@ -7,34 +7,34 @@ import { faUser } from "@fortawesome/free-solid-svg-icons";
 export default function Header() {
   return (
     <header className="flex">
-      <nav className="flex flex-row bg-slate-500">
-        <div className="">
+      <nav className="flex flex-row justify-between px-[5%] h-14 bg-slate-300 items-center w-full">
+        <div className="text-xl">
           <Link to="/" className="">
             AgriConnect
           </Link>
         </div>
 
         <div className="flex flex-row  gap-4">
-          <Link to={"/"} className="text-secondTextColor text-xl font-normal">
+          <Link to={"/"} className="text-secondTextColor text-lg font-normal">
             AgriConnect
           </Link>
 
           <Link
             to={"/harvest-over-crop"}
-            className="text-secondTextColor text-xl font-normal"
+            className="text-secondTextColor text-lg font-normal"
           >
             HarvestOverCrop
           </Link>
 
           <Link
             to={"/bazaar"}
-            className="text-secondTextColor text-xl font-normal"
+            className="text-secondTextColor text-lg font-normal"
           >
             Bazaar
           </Link>
         </div>
 
-        <div className="flex gap-2 text-primaryColor">
+        <div className="flex gap-2 text-primaryColor ">
           <Link to="/profile">
             <FontAwesomeIcon icon={faUser} style={{ color: "primaryColor" }} />
           </Link>
