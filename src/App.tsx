6b1bd2a3b6352f12271cb6ce46073f164348abf2
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-07-04 15:10:15 +0300

    Appy styling to footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import "./App.css";
+import Footer from "./assets/components/Footer";
 import Header from "./assets/components/Header";
 
 function App() {
   return (
     <>
       <Header />
-      
+      <Footer/>
     </>
   );
 }
--- src/assets/components/Footer.tsx
+++ src/assets/components/Footer.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
 
 export default function Footer() {
   return (
-    <Footer>
-        
-    </Footer>
-  )
+    <footer>
+      <div className="flex flex-col px-[5%] bg-slate-300 mt-5">
+        <div className="flex flex-row justify-between">
+          <div className="flex-col">
+            <h3>Get In Touch</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+          </div>
+          <div className="flex-col">
+            <h3>Company Info</h3>
+            <p> About Us</p>
+            <p>Contact Us</p>
+          </div>
+        </div>
+
+        <div className="copy-right">
+          <p className="text-center">Copyright</p>
+        </div>
+      </div>
+    </footer>
+  );
 }
