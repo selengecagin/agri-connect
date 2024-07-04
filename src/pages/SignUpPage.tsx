c77702eb7e72610f8433b48bd02e63028ef256d2
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-07-05 00:20:27 +0300

    Import react-hook-form

--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React from "react";
+import { useForm } from "react-hook-form";
+
 
 export default function SignUpPage() {
+      const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+      } = useForm();
   return (
     <div>
-      
-      
+      <form>
+
+
+
+      </form>
     </div>
-  )
+  );
 }
