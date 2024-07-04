77403b642087cffc6445e0ad1b499b56f6df8730
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-07-04 14:14:45 +0300

    project created and issues solved

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Header from "./assets/components/Header";
 function App() {
   return (
     <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <Header />
     </>
   );
