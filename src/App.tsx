b79455bc641eb23105ce58d59d0549f1e28e1d62
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-07-04 23:50:53 +0300

    Create sign up page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import "./App.css";
 import Footer from "./assets/components/Footer";
 import Header from "./assets/components/Header";
+import SignUpPage from "./pages/SignUpPage";
 
 function App() {
   return (
     <>
       <Header />
+      
+      <SignUpPage/>
+
       <Footer/>
     </>
   );
