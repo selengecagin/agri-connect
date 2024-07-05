5a0271666d18ba7c4c0c0c9437d878a90bfdf407
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-07-05 14:49:49 +0300

    Initialize date-fns library and collect timestamps for forum card

--- src/assets/components/ForumCard.tsx
+++ src/assets/components/ForumCard.tsx
@@ -1,6 +1,14 @@
-import React from "react";
+import { format } from "date-fns";
+import React, { useEffect, useState } from "react";
 
 export default function ForumCard() {
+const [creationTime, setCreationTime] = useState<Date | null>(null);
+
+  useEffect(() => {
+    const now = new Date();
+    setCreationTime(now);
+  }, []);
+
   return (
     <div className="flex flex-row border border-solid">
       <div className="flex flex-col justify-start px-4 py-2 gap-1 w-[150px] items-end text-sm">
@@ -12,11 +20,18 @@ export default function ForumCard() {
       <div className="flex flex-col w-full px-4 py-2 justify-center gap-3 ">
         <div className="flex- flex-col">
           <p className="text-lg text-blue-400">Quastion Title</p>
-          <p>description</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          </p>
         </div>
         <div className="flex flex-row justify-between">
           <p>Tags</p>
-          <p>TimeStamp</p>
+          
+          {creationTime && (
+            <p>{format(creationTime, "yyyy-MM-dd HH:mm:ss")}</p>
+          )}
         </div>
       </div>
     </div>
