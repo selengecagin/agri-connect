019a83269b0d307e4aa76d3c051fb27b3bf95e18
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-07-05 05:48:53 +0300

    Add signup route and 404 Not Found Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Footer from "./assets/components/Footer";
 import Header from "./assets/components/Header";
@@ -7,10 +8,18 @@ function App() {
   return (
     <>
       <Header />
-      
-      <SignUpPage/>
 
-      <Footer/>
+      <Routes>
+        {/* <Route path="/" element={<HomePage />}></Route> */}
+        {/* <Route path="/agri-connect" element={<AgriConnect />}></Route> */}
+        {/* <Route path="/harvest-over-crop" element={<HarvestOverCrop />}></Route> */}
+        {/* <Route path="/bazaar" element={<Bazaar />}></Route> */}
+
+        <Route path="/signup" element={<SignUpPage />}></Route>
+        <Route path="*" element={<h1>404 Not Found Page</h1>} />
+      </Routes>
+
+      <Footer />
     </>
   );
 }
