cfc01634b26aa259e80a3f2cccc094180ea31bd9
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-07-05 16:59:54 +0300

    Add filet button group

--- src/assets/components/ForumCard.tsx
+++ src/assets/components/ForumCard.tsx
@@ -5,7 +5,6 @@ export default function ForumCard() {
 
   return (
     <div className="flex flex-row border border-solid">
-        
       <div className="flex flex-col justify-start px-4 py-2 gap-1 w-[150px] items-end text-sm">
         <p>2 votes</p>
         <p>1 answers</p>
@@ -18,14 +17,14 @@ export default function ForumCard() {
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            ad minim veniam, quis nostrud exercitation ullamco laboris. Lorem
+            ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris.
           </p>
         </div>
         <div className="flex flex-row justify-end">
-
-        
-            <p className="text-sm"> less than 1 minute ago</p>
-        
+          <p className="text-sm"> less than 1 minute ago</p>
         </div>
       </div>
     </div>
--- src/pages/AgriConnect.tsx
+++ src/pages/AgriConnect.tsx
@@ -9,7 +9,28 @@ export default function AgriConnect() {
           <div className="flex flex-col w-full mx-3.5">
             <div className="flex flex-row justify-between items-center h-[50px]">
               <p>10000 questions</p>
-              <div> Filter</div>
+              <div>
+                <div className="flex row rounded-lg border border-gray-300">
+                  <button
+                    className="align-middle select-none font-sans text-center transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none rounded-lg text-xs py-3 px-3 text-gray-900 hover:opacity-75 focus:ring focus:ring-gray-300 active:opacity-[0.85] rounded-r-none border-r-0"
+                    type="button"
+                  >
+                    Newest
+                  </button>
+                  <button
+                    className="align-middle select-none font-sans text-center transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-3 text-gray-900 hover:opacity-75 focus:ring focus:ring-gray-300 active:opacity-[0.85] rounded-r-none border-r-0 rounded-l-none"
+                    type="button"
+                  >
+                    Oldest
+                  </button>
+                  <button
+                    className="align-middle select-none font-sans text-center transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none rounded-lg text-xs py-3 px-3 text-gray-900 hover:opacity-75 focus:ring focus:ring-gray-300 active:opacity-[0.85] rounded-l-none"
+                    type="button"
+                  >
+                    Unanswered
+                  </button>
+                </div>
+              </div>
             </div>
 
             <div className="flex flex-col h-auto">
@@ -20,14 +41,19 @@ export default function AgriConnect() {
         </div>
 
         <div className="side-container flex flex-col w-1/3 mx">
-          <div className="flex flex-col">Tags</div>
+
           <div className="flex flex-col">
+          <p>Related Tags</p>
+          <div className="flex flex-col">
+        <a className="bg-slate-500 rounded-lg inline-block"></a>
+          </div>
+          </div>
+          
+          <div className="flex flex-col">
+
             <h4>Network Questions</h4>
-            <div>
-                
-            </div>
 
-
+            <div></div>
           </div>
         </div>
       </div>
