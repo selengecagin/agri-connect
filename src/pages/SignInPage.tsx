3aee270e26d2f7b753231a4506c26e96b1799a3d
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-07-05 06:00:17 +0300

    Implement email input field with validation using useForm hook

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 
 export default function SignInPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+    const navigate = useNavigate();
+    const [loading, setLoading] = useState(false);
+
   return (
     <div className="">
-      <form>
-
-
-        
+      <form className="">
+        <label htmlFor="email" className="w-[450px]">
+          <p className="pb-2 text-lg font-normal text-darkTextColor">Email</p>
+          <input
+            type="text"
+            placeholder="example@domain.com"
+            className="w-[450px] h-[50px] pl-3 items-center shrink-0 shadow-sm"
+            {...register("email", {
+              required: "Please enter email address.",
+            })}
+          ></input>
+          <p className="text-red-500">{errors.email?.message}</p>
+        </label>
       </form>
     </div>
   );
