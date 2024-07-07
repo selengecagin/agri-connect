0e59acd593bf01ef172a22973e624ed0e454334a
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-07-08 01:29:32 +0300

    Wrap App component with Provider

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -5,11 +5,12 @@ import {
 } from "redux";
 import { thunk } from "redux-thunk";
 import logger from 'redux-logger';
+import userReducer from "./reducers/userReducer";
 
 //eğer bir componente state datası çekmek istiyorsak store içerisinden reducer içersisinden değeri çekmeliyiz
 //örneğin store içerisinden categories çekmek için store.global.categories den almalıyız.
 export const reducers = combineReducers({
-
+ 
 });
 
 export const store = createStore(reducers, applyMiddleware(thunk, logger));
