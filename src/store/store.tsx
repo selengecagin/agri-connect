4eecb936ec1e0d373565027021cc903c624d4412
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-07-08 00:24:02 +0300

    Create user actions and reducer for user state management

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -6,7 +6,8 @@ import {
 import { thunk } from "redux-thunk";
 import logger from 'redux-logger';
 
-
+//eğer bir componente state datası çekmek istiyorsak store içerisinden reducer içersisinden değeri çekmeliyiz
+//örneğin store içerisinden categories çekmek için store.global.categories den almalıyız.
 export const reducers = combineReducers({
 
 });
