0c66ec22e5fc344f2acf12900370933bd47753a9
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-07-08 01:22:47 +0300

    Define initial state for user authentication

---
+++ src/store/reducers/userReducer.tsx
@@ -0,0 +1,10 @@
+import { GlobalUserActions } from "../actions/userActions";
+
+const initialState = {
+  id: null,
+  name: "",
+  userName: "",
+  email: "",
+  isLoggedIn: false,
+  error: null,
+};
