7cb55658213e961ca10f132fde0cfe03d6045f55
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-07-08 00:37:01 +0300

    Define action creators for login success, failure, verify, and exit

--- src/store/actions/userActions.tsx
+++ src/store/actions/userActions.tsx
@@ -5,3 +5,22 @@ export const GlobalUserActions = {
   setLoginExit: "LOGIN_EXIT",
 };
 
+// TODO data:any check
+export const loginSuccess = (data: any) => ({
+  type: GlobalUserActions.setLoginSuccess,
+  payload: data,
+});
+
+export const loginFailure = (data: any) => ({
+  type: GlobalUserActions.setLoginFailure,
+  payload: data,
+});
+
+export const loginVerify = (data: any) => ({
+  type: GlobalUserActions.setLoginVerify,
+  payload: data,
+});
+
+export const loginExit = () => ({
+  type: GlobalUserActions.setLoginExit,
+});
