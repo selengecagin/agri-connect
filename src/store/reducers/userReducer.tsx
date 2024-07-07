f856742c94a5768b8cf229d7a1a5bec810c88e7c
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-07-08 01:23:34 +0300

    Implement userReducer

--- src/store/reducers/userReducer.tsx
+++ src/store/reducers/userReducer.tsx
@@ -8,3 +8,29 @@ const initialState = {
   isLoggedIn: false,
   error: null,
 };
+
+
+const userReducer = (
+  state = initialState,
+  action: {
+    type: any;
+    payload: { id: any; name: any; email: any; role_id: any };
+  }
+) => {
+  switch (action.type) {
+    case GlobalUserActions.setLoginSuccess:
+      return {
+        ...state,
+        id: action.payload.id,
+        name: action.payload.name,
+        email: action.payload.email,
+        isLoggedIn: true,
+        error: null,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default userReducer;
