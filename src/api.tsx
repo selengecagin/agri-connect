6bea5664a60832824743ab6838b79e754581ee38
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-07-08 11:47:26 +0300

    Add base url

--- src/api.tsx
+++ src/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://mock-api-url.com",
+  baseURL: "http://172.16.99.112:8080",
 });
 
 export default api;
--- src/pages/HarvestOverCrop.tsx
+++ src/pages/HarvestOverCrop.tsx
@@ -1,17 +1,16 @@
 import React from "react";
 
 export default function HarvestOverCrop() {
+
+
+
   return (
     <div className="main-container w-full">
       <div className="flex flex-row justify-between px-[5%] py-12 bg-[#fafafa]">
         <div className="scroll-container flex flex-col w-2/3 h-[300px] mx-3.5">
         
         
-        
-        
         </div>
-
-
 
 
         <div className="flex flex-col w-1/3 mx-6">
