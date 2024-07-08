dbc645ee7f4a77678dad29618f98966ce642ceb6
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-07-08 12:26:42 +0300

    Update CSS

--- src/pages/HarvestOverCrop.tsx
+++ src/pages/HarvestOverCrop.tsx
@@ -2,31 +2,23 @@ import React from "react";
 import PostComponent from "../assets/components/PostComponent";
 
 export default function HarvestOverCrop() {
-
-
-
   return (
     <div className="main-container w-full">
-      <div className="flex flex-row justify-between px-[5%] py-12 bg-[#fafafa]">
-        <div className="scroll-container flex flex-col w-2/3 mx-3.5">
-    
-    <PostComponent/>
-        
+      <div className="flex flex-row justify-between px-[5%] py-16 bg-[#fafafa]">
+        <div className="scroll-container flex flex-col w-2/3 mx-3.5 py-4">
+          <PostComponent />
         </div>
 
+        <div className="flex flex-col w-1/3 mx-6 mt-8 p-6 border rounded-md  justify-start shadow-lg">
+          <div className="flex flex-col gap-4">
+            <h1 className="text-lg text-[#737373] font-bold">
+              Recommended Contacts
+            </h1>
 
-        <div className="flex flex-col w-1/3 mx-6">
-          <div className="flex flex-col">
-
-            <h3>Recommended Contacts</h3>
-
-            <div className="">
-                <img></img>
-                <p>Username</p>
+            <div className="flex flex-row gap-2">
+              <p>Icon</p>
+              <p>Username</p>
             </div>
-
-
-
           </div>
         </div>
       </div>
