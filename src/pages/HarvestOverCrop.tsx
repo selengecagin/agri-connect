bd69e093cdaadaeef0f6941b66d043c74f9a2171
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-07-08 11:43:34 +0300

    Implement react infinite scroll component

--- src/pages/HarvestOverCrop.tsx
+++ src/pages/HarvestOverCrop.tsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from "react";
 
 export default function HarvestOverCrop() {
   return (
-    <div>
-
+    <div className="main-container w-full">
+      <div className="flex flex-row justify-between px-[5%] py-12 bg-[#fafafa]">
+        <div className="scroll-container flex flex-col w-2/3 h-[300px] mx-3.5">
         
-      
+        
+        
+        
+        </div>
+
+
+
+
+        <div className="flex flex-col w-1/3 mx-6">
+          <div className="flex flex-col">
+
+            <h3>Recommended Contacts</h3>
+
+            <div className="">
+                <img></img>
+                <p>Username</p>
+            </div>
+
+
+
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
