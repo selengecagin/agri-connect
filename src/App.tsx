96738e614da261e52a4e5fd9bc2dadd27196ac6e
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-07-08 13:21:52 +0300

    Fix errors

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ import SignUpPage from "./pages/SignUpPage";
 import SignInPage from "./pages/SignInPage";
 import AgriConnect from "./pages/AgriConnect";
 import HarvestOverCrop from "./pages/HarvestOverCrop";
+import ProfileSettingsPage from "./pages/ProfileSettingsPage";
+import ProfilePage from "./pages/ProfilePage";
+import AskQuestionPage from "./pages/AskQuestionPage";
 
 function App() {
   return (
@@ -19,14 +22,23 @@ function App() {
 
         <Route path="/signup" element={<SignUpPage />}></Route>
         <Route path="/signin" element={<SignInPage />}></Route>
-        <Route path="/agri-connect" element={<AgriConnect />}></Route>
-        <Route path="/harvest-over-crop" element={<HarvestOverCrop />}></Route>
+        <Route path="/agri-connect" element={<HarvestOverCrop />}></Route>
+        <Route path="/harvest-over-crop" element={<AgriConnect />}></Route>
+        <Route path="/ask-question" element={<AskQuestionPage />} />
+        <Route
+          path="/profile-settings"
+          element={<ProfileSettingsPage />}
+        ></Route>
+        <Route path="/profile-page" element={<ProfilePage />}></Route>
+
         <Route path="*" element={<h1>404 Not Found Page</h1>} />
       </Routes>
 
       <Footer />
     </>
   );
+}{
+
 }
 
 export default App;
